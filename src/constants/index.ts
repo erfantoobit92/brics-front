6b92c21e7d8445f672575828e68cbca0731030e1
Erfun <erfanhasanzadegorji@gmail.com>
2025-09-10 21:00:22 +0330

    fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 // export const Main_API_URL = "http://192.168.221.132:3000";
-export const Main_API_URL = "http://127.0.0.1:3000";
-// export const Main_API_URL = "https://api.mohammaditest.ir";
-export const WEB_APP_URL = "https://jemjoo.erfuni.ir";
+// export const Main_API_URL = "http://127.0.0.1:3000";
+export const Main_API_URL = "https://api.mohammaditest.ir";
+export const WEB_APP_URL = "https://game.mohammaditest.ir";
 
 export const Telegram_Bot_Url = "t.me/Bricstrade_Bot/Brics_Trade";
 export const Telegram_Bot_Username = "Bricstrade_Bot";
